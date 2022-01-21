7c0c91b2bf17b8418ecabdf57e1f991fb835f93c
Toufeeq Pasha <47236805+ToufeeqP@users.noreply.github.com>
2022-01-21 18:47:50 +0530

    Update top-languages-fetcher.js

--- src/fetchers/top-languages-fetcher.js
+++ src/fetchers/top-languages-fetcher.js
@@ -8,8 +8,8 @@ const fetcher = (variables, token) => {
       query: `
       query userInfo($login: String!) {
         user(login: $login) {
-          # fetch only owner repos & not forks
-          repositories(ownerAffiliations: OWNER, isFork: false, first: 100) {
+          # fetch all contributed repos except forks
+          repositories(ownerAffiliations: COLLABORATOR, isFork: false, first: 100) {
             nodes {
               name
               languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
